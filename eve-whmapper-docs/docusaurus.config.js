3124ae0cd295fcae7d1912489f58537fcfc3649a
pfhuillet <pfhuillet@gmail.com>
2024-05-14 18:08:29 +0200

    fix config

--- eve-whmapper-docs/docusaurus.config.js
+++ eve-whmapper-docs/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com/',
+  url: 'https://github.com/pfh59/eve-whmapper-docs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/eve-whmapper-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
